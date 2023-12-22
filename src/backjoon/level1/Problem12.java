21b220971c5455bc9f84be569fe27239bbe5d4a7
Mdragon <minyong_e89@naver.com>
2023-12-22 23:58:53 +0900

    problem 12 ~ 13

---
+++ src/backjoon/level1/Problem12.java
@@ -0,0 +1,10 @@
+package backjoon.level1;
+
+public class Problem12 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
