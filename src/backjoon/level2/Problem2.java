4de833ce765d313ae13cc020b8c6cbc4721c4ccb
Mdragon <minyong_e89@naver.com>
2023-12-23 18:03:21 +0900

    problem 1 ~ 2

---
+++ src/backjoon/level2/Problem2.java
@@ -0,0 +1,28 @@
+package backjoon.level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String sts = br.readLine();
+
+        int resultNum = Integer.parseInt(sts);
+
+        if(resultNum >= 90 && resultNum <= 100){
+            System.out.println("A");
+        } else if (resultNum >= 80 && resultNum <= 89) {
+            System.out.println("B");
+        } else if (resultNum >= 70 && resultNum <= 79) {
+            System.out.println("C");
+        } else if (resultNum >= 60 && resultNum <= 69) {
+            System.out.println("D");
+        }else {
+            System.out.println("F");
+        }
+    }
+}
