a260957943c2084802d018b51e615908db910be7
MinYongUm <minyong_e89@naver.com>
2024-01-05 17:01:09 +0900

    Problem 6 ~ 7 Update

---
+++ src/backjoon/level2/Problem6.java
@@ -0,0 +1,27 @@
+package backjoon.level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strList = br.readLine().split(" ");
+        String str = br.readLine();
+
+        int hour = Integer.parseInt(strList[0]);
+        int min = Integer.parseInt(strList[1]);
+        int ovenTime = Integer.parseInt(str);
+
+        int totalMin = 60 * hour + min;
+        totalMin += ovenTime;
+
+        int resultHour = (totalMin / 60) % 24;
+        int resultMin = totalMin % 60;
+
+        System.out.println(resultHour + " " + resultMin);
+
+    }
+}
---
+++ src/backjoon/level2/Problem7.java
@@ -0,0 +1,52 @@
+package backjoon.level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strList = br.readLine().split(" ");
+
+        int a = Integer.parseInt(strList[0]);
+        int b = Integer.parseInt(strList[1]);
+        int c = Integer.parseInt(strList[2]);
+        int result = 0;
+
+        if(a != b && b != c && a != c){
+            int max;
+
+            if(a > b){
+                if(c > a){
+                    max = c;
+                } else {
+                    max = a;
+                }
+            } else {
+                if(c > b){
+                    max = c;
+                } else {
+                    max = b;
+                }
+            }
+            result = max * 100;
+            System.out.println(result);
+
+        } else {
+            if(a == b && a == c){
+                result = 10000 + a * 1000;
+                System.out.println(result);
+            } else {
+                if(a == b || a == c){
+                    result = 1000 + a * 100;
+                    System.out.println(result);
+                }else {
+                    result = 1000 + b * 100;
+                    System.out.println(result);
+                }
+            }
+        }
+    }
+}
